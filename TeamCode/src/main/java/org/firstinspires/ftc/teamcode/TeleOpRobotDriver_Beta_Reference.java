d430f2eb1e0f4fb1ecd31fe6aea0b10869ad0359
isong <iansong26@gmail.com>
2018-09-26 16:26:21 -0500

    ian set up complete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRobotDriver_Beta_Reference.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRobotDriver_Beta_Reference.java
@@ -18,7 +18,7 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name="TeleOpRobotDriver_Mk_III")
 public class TeleOpRobotDriver_Beta_Reference extends LinearOpMode {
 
-    // Declare OpMode members. maybe this'll work alex was her
+    // Declare OpMode members. maybe this'll work ian was here
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftDrive = null;
     private DcMotor frontRightDrive = null;
